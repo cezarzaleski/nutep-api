44e7ddce529eeaf08ca644550d847385e0c1a0d6
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2021-11-13 19:03:00 -0300

    fix: throw UnauthorizedError in AuthController

--- src/authenticate/infra/controller/auth-controller.ts
+++ src/authenticate/infra/controller/auth-controller.ts
@@ -1,10 +1,11 @@
 import Login from 'src/authenticate/application/login';
 import UserRepositoryDatabase from 'src/authenticate/infra/database/repository/user-repository-database';
 import { JwtAdapter } from 'src/authenticate/infra/cryptography/jwt-adapter';
-import { badRequest, ok, serverError } from 'src/shared/infra/http/http';
+import { badRequest, ok, serverError, unauthorized } from 'src/shared/infra/http/http';
 import InvalidEmailError from 'src/shared/exception/invalid-email';
 import EmptyParamError from 'src/shared/exception/empty-param';
 import NotFoundError from 'src/shared/exception/not-found';
+import UnauthorizedError from 'src/authenticate/domain/exception/unauthorized';
 
 export default class AuthController {
   constructor(
@@ -19,6 +20,7 @@ export default class AuthController {
       return ok({token})
     }  catch (error) {
       if (error instanceof EmptyParamError || error instanceof InvalidEmailError || error instanceof NotFoundError) return badRequest(error)
+      if (error instanceof UnauthorizedError) return unauthorized(error)
       return serverError(error)
     }
   }
