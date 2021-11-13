f85481640eaa922dccc3f87bbaf632f776997b5e
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2021-11-13 00:34:51 -0300

    feat: test throw to invalid-email

--- test/authenticate/domain/entity/user.spec.ts
+++ test/authenticate/domain/entity/user.spec.ts
@@ -3,6 +3,7 @@ import { Email } from 'src/shared/domain/value-object/email';
 
 
 test("Should create a new user", () => {
-    const professional = new User(Email.create('cezar.zaleski@gmail.com'), 'dummy');
-    expect(professional).toBeDefined()
+    const user = new User(Email.create('cezar.zaleski@gmail.com'), 'dummy');
+    expect(user.email).toEqual(Email.create('cezar.zaleski@gmail.com'))
+    expect(user.password).toEqual('dummy')
 });
--- test/shared/domain/value-object/email.spec.ts
+++ test/shared/domain/value-object/email.spec.ts
@@ -1,4 +1,5 @@
 import { Email } from 'src/shared/domain/value-object/email';
+import InvalidEmailError from 'src/shared/exception/invalid-email';
 
 
 describe('Email',  () => {
@@ -6,5 +7,9 @@ describe('Email',  () => {
   it('Should create email valid', () => {
     const email = Email.create('cezar.zaleski@gmail.com');
     expect(email.value).toBe('cezar.zaleski@gmail.com')
+  })
+
+  it('Should throw to invalid-email', () => {
+    expect(() => Email.create('cezar.zaleski')).toThrow(new InvalidEmailError("cezar.zaleski"));
   })
 })
