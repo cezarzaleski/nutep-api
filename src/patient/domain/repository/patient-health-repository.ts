15c21e2bbcc6321eadadc249f727e8207f032145
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2022-01-28 23:20:23 -0300

    refactor: delete patient-health repository

--- src/patient/domain/repository/patient-health-repository.ts
+++
@@ -1,5 +0,0 @@
-import PatientHealth from 'src/patient/domain/entity/patient-health';
-
-export default interface PatientHealthRepository {
-  save(patientHealth: PatientHealth): Promise<PatientHealth>
-}
